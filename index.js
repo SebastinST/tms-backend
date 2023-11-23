3a0c7ed9d0f9b54511c61e6e8505f5c7e22084ae
SebastinST <mingchensebastin.goh@stengg.com>
2023-11-23 15:36:24 +0800

    add pseudo code for assignment 1

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const session = require('express-session');
-const app = express();
+const router = express.Router();
 const port = 3000;
 
 // Inititalize the app and add middleware
+const app = express();
 app.use(express.json());
 app.use(session({secret: 'super-secret'})); // Session setup
 
-/** U1: Login & Logout **/ 
-// Login interface
-app.get('/login', (req, res) => {
-  if (req.session.isLoggedIn === true) {
-    return res.redirect('/');
-  }
-  res.render('login', {error: false});
-});
 
-// Login attempt
+/** U1: Login & Logout **/ 
+
+// User login
+// POST to '/login'
 app.post('/login', (req, res) => {
   const {username, password} = req.body;
+
+  // DB select user account based on 'username'
+    // Check for valid user account
+    
+  // Check user account password != provided password
+    
+  // Return login success
+
   if (username === 'bob' && password === '1234') {
     req.session.isLoggedIn = true;
     res.redirect(req.query.redirect_url ? req.query.redirect_url : '/');
@@ -27,28 +31,108 @@ app.post('/login', (req, res) => {
   }
 });
 
-// Logout interface
+// User logout
+// GET to '/logout'
 app.get('/logout', (req, res) => {
+  // Remove cookies
   req.session.isLoggedIn = false;
-  res.redirect('/');
+
+  // Return logout success
 });
 /** End of U1: Login & Logout **/ 
 
 /** U2: Update Account **/ 
-// Account Interface
-app.get('/', (req, res) => {
-  res.render('index', {isLoggedIn: req.session.isLoggedIn});
+// Show current user account details
+// GET to '/getSelf'
+app.get('/getSelf', (req, res) => {
+  
+  // Get username from token
+
+  // DB select user account details based on username
+  
+  // Return user account details
 });
 
-// Account updating
+// Update current user account details
+// POST to '/updateSelf'
+app.post('/updateSelf', (req, res) => {
+  const {email, password} = req.body;
 
+  // Get username from token
+
+  // Check if email is provided, it is valid 
+  // Check if password is provided, it is valid
+
+  // Check if either email or password is provided
+    // DB update user account details based on username
+      // if email is given, update email
+      // if password is given, update password
+  
+  // Return successful update
+});
 /** End of U2: Update Account **/ 
 
 /** A1: Manage Accounts **/ 
-// Admin Interface
+// Create user group
+// POST to '/createGroup'
+app.post('/createGroup', (req, res) => {
+  const {group_name} = req.body;
 
-// Manage Accounts
+  // Check if group_name is valid
 
+  // DB create group with group_name
+
+  // Return successful creation
+
+})
+
+// Create user account
+// POST to '/createUser'
+app.post('/createUser', (req, res) => {
+  const { username, password, email, group_list} = req.body;
+  
+  // Check username and password provided and valid
+
+  // DB create user with given details
+
+  // Return successful creation
+});
+
+// Show all user account details
+// GET to '/getAllUsers'
+app.get('/getAllUsers', (req, res) => {
+  
+  // DB select all users account details
+
+  // Return all users account details
+
+});
+
+// Update user account
+// POST to '/updateUser'
+app.post('/updateUser', (req, res) => {
+  const {username, email, password, group_list} = req.body;
+
+  // Check password is provided and valid
+
+  // DB update user account details based on username
+    // if email is given, update email
+    // if password is given, update password
+  
+  // Return successful update
+
+});
+
+// Toggle user account status
+// POST to '/toggleUserStatus'
+app.post('/toggleUserStatus', (req, res) => {
+  const {username} = req.body;
+
+  // DB toggle user account status based on username
+
+  // Return successful update
+
+});
 /** End of A1: Manage Accounts **/ 
 
 
