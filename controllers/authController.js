c5c901b859508422ecf72de366452d1f1fc89260
SebastinST <mingchensebastin.goh@stengg.com>
2023-11-24 13:38:19 +0800

    U1 clean up api end point for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,10 +14,11 @@ exports.login = async (req, res) => {
             "SELECT * FROM user WHERE username=?", 
             [username]
         )
+
+        const user = result[0][0];
         
-        // Check for valid user account
-        // CHECK FOR IS_DISABLED
-        if (result[0].length === 0) {
+        // Check for valid user account, and password
+        if (!user || user.is_disabled || password != user.password) {
             res.status(400).json({
                 success : false,
                 message : 'Error: Invalid login',
@@ -25,37 +26,24 @@ exports.login = async (req, res) => {
             return;
         };
 
-        const user = result[0][0];
+        // Handle valid login
 
-        // Check valid credentials and not disabled account
-        if (password == user.password && !user.is_disabled) {
-            
-            // Add token
-            const token = jwt.sign(
-                {username: user.username}, 
-                process.env.JWT_SECRET, 
-                {expiresIn: process.env.JWT_EXPIRES_TIME}
-            )
-            // Options for cookie
-            const options = {
-                expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
-                httpOnly: true
-            }
-
-            res.status(200).cookie("token", token, options).json({
-                success: true,
-                token,
-                group_list: user.group_list
-            })
-            
-        } else {
-            res.status(400).json({
-                success : false,
-                message : 'Error: Invalid login',
-            });
-            return;
+        const token = jwt.sign(
+            {username: user.username}, 
+            process.env.JWT_SECRET, 
+            {expiresIn: process.env.JWT_EXPIRES_TIME}
+        )
+        const options = {
+            expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
+            httpOnly: true
         }
 
+        res.status(200).cookie("token", token, options).json({
+            success: true,
+            token,
+            group_list: user.group_list
+        })
+        
     } catch(e) {
         res.status(500).json({
             success : false,
