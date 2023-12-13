8886464649b12c34e0dd215fd3aa9796ccfa6ea9
SebastinST <mingchensebastin.goh@stengg.com>
2023-12-13 11:29:07 +0800

    Assignment 2 add routing permission for PL to get all groups

--- router.js
+++ router.js
@@ -38,7 +38,7 @@ router.route("/updateSelf").post(isUserValid, updateSelf);
 router.route("/createGroup").post(isUserValid, authorizedGroups("admin"), createGroup);
 router.route("/createUser").post(isUserValid, authorizedGroups("admin"), createUser);
 router.route("/getAllUsers").get(isUserValid, authorizedGroups("admin"), getAllUsers);
-router.route("/getAllGroups").get(isUserValid, authorizedGroups("admin"), getAllGroups);
+router.route("/getAllGroups").get(isUserValid, authorizedGroups("admin", "pl"), getAllGroups);
 router.route("/updateUser").post(isUserValid, authorizedGroups("admin"), protectAdmin, updateUser);
 router.route("/toggleUserStatus").post(isUserValid, authorizedGroups("admin"), protectAdmin, toggleUserStatus);
 router.route("/Checkgroup").post(isUserValid, checkingGroup);
